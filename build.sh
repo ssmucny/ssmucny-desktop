15d6c61eb360ffb643bc332469ad0d32f0d78f94
Sam Smucny <smucny.sam@gmail.com>
2024-12-26 18:01:02 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,7 +13,6 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 rpm-ostree install corectrl # install for CPU/GPU power control
-rpm-ostree kargs --append-if-missing="amdgpu.ppfeaturemask=0xffffffff" # karg for corectl on AMD GPUs
 
 #### Example for enabling a System Unit File
 
