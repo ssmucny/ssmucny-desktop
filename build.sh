2ea1a85ad2d03ab5f8b4f1e0a48c46317b639846
Sam Smucny <smucny.sam@gmail.com>
2025-01-13 23:02:22 -0500

    IME packages

--- build.sh
+++ build.sh
@@ -12,7 +12,11 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-rpm-ostree install corectrl # install for CPU/GPU power control
+# install for CPU/GPU power control
+rpm-ostree install corectrl
+
+# IME packages
+rpm-ostree install fcitx5 fcitx5-gtk fcitx5-qt fcitx5-configtool fcitx5-mozc fcitx5-autostart imsettings im-chooser
 
 #### Example for enabling a System Unit File
 
