2129e71d39a57d0bfb624c612c062382a8a0b042
Sam Smucny <smucny.sam@gmail.com>
2025-12-29 18:05:13 -0500

    Update build.sh
    Updated comments and removed unnecessary RPM installations.

--- build.sh
+++ build.sh
@@ -12,26 +12,17 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# install for CPU/GPU power control
+# install for CPU/GPU power control (VR utility)
 rpm-ostree install corectrl
 
 # install VM runtimes and SDK things
 rpm-ostree install java-latest-openjdk dotnet-sdk-8.0 dotnet-sdk-9.0 dotnet-sdk-10.0 nodejs
 
 # IME packages (Japanese language input)
-#rpm-ostree install fcitx5 fcitx5-gtk fcitx5-qt fcitx5-configtool fcitx5-mozc fcitx5-autostart imsettings im-chooser
+rpm-ostree install fcitx5-autostart fcitx5-configtool
 
 # Tools
 
 rpm-ostree install graphviz
-
-## VS Codium
-
-# rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
-# rpm-ostree install codium
-
-# rpm-ostree install libappindicator-gtk3 libappindicator netbird-ui
-
-#### Example for enabling a System Unit File
 
 systemctl enable podman.socket
